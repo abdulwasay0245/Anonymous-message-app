53eabc7c05afe2627845d4e34ae7e087139c5e02
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-18 23:56:04 +0500

    added dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { Message } from '@/model/user'
 import { acceptMessageSchema } from '@/schemas/acceptMessageSchema'
+import { apiResponse } from '@/types/apiResponse'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
 import { useSession } from 'next-auth/react'
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 const Dashboard = () => {
@@ -18,6 +20,19 @@ const Dashboard = () => {
     const form = useForm({
         resolver: zodResolver(acceptMessageSchema)
     })
+    const { register, watch, setValue } = form;
+    const acceptMessage = watch('acceptMessages')
+    const fetchedAcceptMessage = useCallback(async () => {
+        setisSwitchLoading(true)
+        try {
+            const response = await  axios.get('/api/accept-messages')
+            setValue('acceptMessage', response.data.isAcceptingMessage)
+        } catch (error) {
+            const axiosError = error as AxiosError<apiResponse>
+            toast(axiosError.response?.data.message || " Failed to fetch message settings")
+        }
+    },[setValue])
+
  
   return (
     <div>Dashboard</div>
