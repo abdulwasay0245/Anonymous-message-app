98ddf83c42502fa76216015252aae0326bb9719e
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-19 09:24:53 +0500

    added dashboard logics

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { apiResponse } from '@/types/apiResponse'
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios, { AxiosError } from 'axios'
 import { useSession } from 'next-auth/react'
-import {useCallback, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 const Dashboard = () => {
@@ -21,7 +21,7 @@ const Dashboard = () => {
         resolver: zodResolver(acceptMessageSchema)
     })
     const { register, watch, setValue } = form;
-    const acceptMessage = watch('acceptMessages')
+    const acceptMessage = watch('acceptMessage')
     const fetchedAcceptMessage = useCallback(async () => {
         setisSwitchLoading(true)
         try {
@@ -31,9 +31,55 @@ const Dashboard = () => {
             const axiosError = error as AxiosError<apiResponse>
             toast(axiosError.response?.data.message || " Failed to fetch message settings")
         }
-    },[setValue])
+        finally {
+            setisSwitchLoading(false)
+        }
+    }, [setValue])
+    const fetchMessages = useCallback(async (refresh: boolean = false) => {
+        setIsLoading(true)
+        setisSwitchLoading(false)
+        try {
+            const response = await axios.get<apiResponse>('/api/get-messages')
+            setMessage(response.data.messages || [])
+            if (refresh) {
+                toast('showing lates messages')
+            }
+            
+        } catch (error) {
+            const axiosError = error as AxiosError<apiResponse>
+            toast(axiosError.response?.data.message || " Failed to fetch message settings")
+        }
+        finally {
+            setIsLoading(false)
+            setisSwitchLoading(false)
+        }
+    },[setIsLoading,setMessage])
 
- 
+    useEffect(() => {
+        if (!session || !session.user) return
+        fetchMessages();
+        fetchedAcceptMessage();
+    }, [session, setValue, fetchMessages, fetchedAcceptMessage])
+    
+    // handling switch change
+    const handleSwitchChance = async () => {
+        try {
+            const response = await axios.post<apiResponse>('/api/accept-messages', {
+                acceptMessage: !acceptMessage
+            })
+            setValue('acceptMessage', !acceptMessage)
+            toast(response.data.message)
+        } catch (error) {
+            const axiosError = error as AxiosError<apiResponse>;
+            toast(axiosError.response?.data.message || "failed to fetch")
+
+        }
+        if (!session || !session.user) {
+            return <div>Please login</div>
+        }
+    }
+
+
   return (
     <div>Dashboard</div>
   )
