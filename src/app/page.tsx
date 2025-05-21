397cf4d1bbdde8ce024354e05ce39c7f893aefda
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-21 11:24:38 +0500

    added carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
-
-import { Camera } from 'lucide-react';
-
+'use client'
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
 export default function Home() {
   
-
-  // Usage
+  return (
   
-    return <Camera color="red" size={48} />;
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">Dive into the World of Anonymous Conversation</h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">Explore Mystery Message - Where your identity remains a secret.</p>
+      </section>
+      <Carousel plugins={[Autoplay({delay: 2000})] } className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    </main>
+
+)
+
   
 }
